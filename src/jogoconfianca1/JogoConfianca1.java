fe4a80fbdf79734610ba46d5d0071c243660ed1b
alistonroier <aliston.p@icloud.com>
2021-06-09 00:11:53 -0300

    Primeiro commit

--- src/jogoconfianca1/JogoConfianca1.java
+++ src/jogoconfianca1/JogoConfianca1.java
@@ -4,9 +4,6 @@ package jogoconfianca1;
 
 public class JogoConfianca1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
